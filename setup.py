71a5c5da1ca2a0606a38d4b8a86a23f131ec3186
IrmuunSolo <Oyutan6677@gmail.com>
2025-03-17 00:53:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,18 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 # ##########################################################
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup( 
+        name="flashcards-cli",
+        version="0.1",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            "click",          # CLI үүсгэхэд шаардлагатай
+            "flashcards-core" # flashcards-core сангаас хамаарна
+        ],
+        entry_points={
+            "console_scripts": [
+                "flashcards=flashcards_cli.main:main",  # CLI комманд
+            ],
+        },
+        python_requires=">=3.8",  # Python 3.8 ба түүнээс дээш шаардлагатай
+)
